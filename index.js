c9134e7f5174a678176fed48811ee40ff4267bc7
LukeHTHompson <lht14dv@icloud.com>
2021-09-02 14:08:00 -0400

    API Endpoints Enabled via Mongoose
    All Endpoints have been given proper logic to update the database as necessary and supply users with appropriate responses to outline changes made or provide requested data.
    
    Documentation file edited to reflect new API Endpoint behaviors.

--- index.js
+++ index.js
@@ -1,52 +1,21 @@
+/* eslint-disable */
+const mongoose = require("mongoose");
+const Models = require("./models.js");
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect("mongodb://localhost:27017/myCinemaDB", { useNewUrlParser: true, useUnifiedTopology: true });
+
 const express = require("express"),
-  morgan = require("morgan");
+  morgan = require("morgan"),
+  bodyParser = require("body-parser");
+/* eslint-enable */
 
 const app = express();
-
-let topTenFilms = [
-  {
-    title: "Catch Me If You Can",
-    director: "Steven Spielberg"
-  },
-  {
-    title: "Wall-E",
-    director: "Andrew Stanton"
-  },
-  {
-    title: "Up",
-    director: "Pete Doctor"
-  },
-  {
-    title: "The Tomorrow War",
-    director: "Chris McKay"
-  },
-  {
-    title: "The Suicide Squad",
-    director: "James Gunn"
-  },
-  {
-    title: "PAW Patrol: The Movie",
-    director: "Cal Brunker"
-  },
-  {
-    title: "Free Guy",
-    director: "Matt Lieberman"
-  },
-  {
-    title: "Narco Sub",
-    director: "Shawn Welling"
-  },
-  {
-    title: "Black Widow",
-    director: "Cate Shortland"
-  },
-  {
-    title: "The Shawshank Redemption",
-    director: "Frank Darabont"
-  }
-]
-
-// Middleware
+// Enable use of bodyParser functionality in all Endpoints
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 // Serve Documentation file as static from the public folder
 app.use(express.static("public"));
 // Log info via Morgan's "combined" pre-set format
@@ -58,50 +27,154 @@ app.get("/", (req, res) => {
   res.send("Welcome to MyCinema!");
 });
 
-// Excercise 2.5 Additions
 // Returns a list of all movies in the database.
 app.get("/movies", (req, res) => {
-  res.send("GET all movies in database")
+  Movies.find()
+  .then( (movies) => {
+    res.status(201).json(movies);
+  })
+  .catch( (err) => {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
 });
 
 // Returns info on a particular movie via title.
-app.get("/movies/:title", (req, res) => {
-  res.send("GET info on one movie specified by title")
+app.get("/movies/:Title", (req, res) => {
+  Movies.findOne({ Title: req.params.Title})
+  .then( (movie) => {
+    res.status(201).json(movie);
+  })
+  .catch( (err) => {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  })
 });
 
-// Adds a movie to a user's list of favorites.
-app.post("/users/:username/favorites/:title", (req, res) => {
-  res.send("POST a new movie to a user's list of favorites by title")
+// Adds a movie by databse ID to a user's list of favorites.
+app.post("/users/:Username/favorites/:MovieID", (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username },
+    { $addToSet: {FavoriteMovies: req.params.MovieID} },
+    { new: true },
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      } else {
+        res.json(updatedUser);
+      }
+    }
+  );
 });
 
-// Removes a movie from a user's list of favorites.
-app.delete("/users/:username/favorites/:title", (req, res) => {
-  res.send("DELETE an existing movie from a user's list of favorites by title")
+// Removes a movie by database ID from a user's list of favorites.
+app.delete("/users/:Username/favorites/:MovieID", (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username },
+    { $pull: {FavoriteMovies: req.params.MovieID} },
+    { new: true },
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      } else {
+        res.json(updatedUser);
+      }
+    }
+  );
 });
 
 // Returns info on a specific film genre.
-app.get("/genres/:genre", (req, res) => {
-  res.send("GET info on a genre of film by name/title")
+app.get("/genres/:Genre", (req, res) => {
+  Movies.find({"Genre.Name": req.params.Genre},
+  {"Genre.Name":1, "Genre.Description":1})
+    .then( (genre) => {
+      res.status(201).json(genre);
+    })
+    .catch( (err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    })
 });
 
 // Returns data on a specific director.
-app.get("/directors/:director", (req, res) => {
-  res.send("GET info on a director by name")
+app.get("/directors/:Director", (req, res) => {
+  Movies.find({"Director.Name": req.params.Director},
+  {"Director.Name":1,"Director.Bio":1,"Director.Birth":1,"Director.Death":1})
+    .then( (director) => {
+      res.status(201).json(director);
+    })
+    .catch( (err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    })
 });
 
 // Create your own account with a custom username.
 app.post("/users", (req, res) => {
-  res.send("POST a new account to the site with info in your request body")
+  Users.findone({ Username: req.body.Username })
+    .then( (name) => {
+      // 'name' is a boolean, true if we found that username in the DB, else false
+      if (name) {
+        return res.status(400).send(req.body.Username + "already exists.");
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: req.body.Passwaord,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then( (user) => {res.status(201).json(user)})
+        // handle any errors in user creation attempt
+        .catch( (error) => {
+          console.error(error);
+          res.status(500).send("Error: " + error);
+        })
+      }
+    })
+    // handle any errors in DB search attempt
+    .catch( (error) => {
+      console.error(error);
+      res.status(500).send("Error: " + error);
+    });
 });
 
-// Update your own username.
-app.put("/users/:username", (req, res) => {
-  res.send("PUT an update to your username")
+// Update user info.
+app.put("/users/:Username", (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username },
+  { $set:
+      { Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday
+      }
+  },
+  { new: true }, // makes the return of our callback function the updated document,
+                 // as opposed to the original (callback function is next line)
+  (err, updatedUser) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
 });
 
 // Delete your own account.
-app.delete("/users/:username", (req, res) => {
-  res.send("DELETE your own account")
+app.delete("/users/:Username", (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+    .then( (user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + " was not found.");
+      } else {
+        res.status(200).send(req.params.Username + " was successfully deleted.");
+      }
+    })
+    .catch( (err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
 // Test for error handler
@@ -109,8 +182,8 @@ app.delete("/users/:username", (req, res) => {
 //   throw new Exception();
 // });
 
-// Handle any errors
-app.use((err, req, res, next) => {
+// Handle any errors in the API as a whole
+app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
