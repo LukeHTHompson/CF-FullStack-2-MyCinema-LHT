c77e40d14f76d8b786d11000ed579ccb6c902b1e
LukeHTHompson <lht14dv@icloud.com>
2022-04-15 17:48:53 -0400

    Add Application to CORS

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
 // Use CORS with the following sites' requests being accepted
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'http://localhost:4200'];
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'http://localhost:4200', 'https://lukehthompson.github.io'];
 
 app.use(cors({
   origin: (origin, callback) => {
